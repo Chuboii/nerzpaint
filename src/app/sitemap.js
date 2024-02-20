08f5c7094b4d7e8046eb9d61ea130963e6b0cf37
Chuboii <favoronyechere@gmail.com>
2024-02-20 15:41:46 +0100

    commit

---
+++ src/app/sitemap.js
@@ -0,0 +1,27 @@
+
+
+export default async function sitemap() {
+
+    return [
+        {
+            url: 'https://nerzpaints.com',
+            lastModified: new Date()
+        },
+        {
+            url: 'https://nerzpaints.com/about',
+            lastModified: new Date()
+        },
+        {
+            url: 'https://nerzpaints.com/product-cards',
+            lastModified: new Date()
+        },
+        {
+            url: 'https://nerzpaints.com/reviews',
+            lastModified: new Date()
+        },
+        {
+            url: 'https://nerzpaints.com/contact',
+            lastModified: new Date()
+        },
+    ]
+}
