927de8f086dda10281ee0d3be04374fa3ae7c23a
Onyechere Chukwuemeka Favour <81271549+Chuboii@users.noreply.github.com>
2024-03-14 09:28:42 +0100

    Update WelcomeText.tsx

--- src/components/welcome section/welcome text/WelcomeText.tsx
+++ src/components/welcome section/welcome text/WelcomeText.tsx
@@ -4,14 +4,14 @@ import {Container,Bold, Text, Title, Wrap, Span, SubText } from './WelcomeText.s
 const WelcomeText = () => {
   return (
       <Container className="w-t-section">
-          <Text className='motto'>Creativity & artistry</Text>
+        {/*  <Text className='motto'>Creativity & artistry</Text>*/}
           <Title className='title'>
               <Wrap>
                 <Span className="light">Painting the future </Span>
             </Wrap>
-              <Bold className="bold">
-               Best Painting company
-              </Bold>
+            {/*<Bold className="bold">
+               The Best Painting company
+              </Bold>*/}
           </Title>
           <SubText className="w-p">An exclusive paint retail brand</SubText>
      <WelcomeButtons/>
