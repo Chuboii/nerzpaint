a30dcadcd1c7f68bf2fd12e63a7524334dbcba57
Onyechere Chukwuemeka Favour <81271549+Chuboii@users.noreply.github.com>
2024-03-14 09:42:20 +0100

    Update QualitySection.tsx

--- src/components/quality section/QualitySection.tsx
+++ src/components/quality section/QualitySection.tsx
@@ -18,7 +18,7 @@ const QualitySection = () => {
               <Image src="/area-line.png" alt="" fill />
           </ImageBoxLine>
            <Wrap>
-            <Text>Nerzpaint emerges as a dynamic force within the paint industry, offering a comprehensive range of solutions tailored for diverse needs. Specializing in interior and exterior paints, alongside a dedicated line of Marine paints, our company stands at the forefront of innovation and quality craftsmanship. With a commitment to excellence and customer satisfaction, Nerzpaint sets a new standard of reliability and performance in every brushstroke, making spaces come alive with vibrant colors and enduring beauty.</Text>      
+            <Text>Nerzpaints emerges as a dynamic force within the paint industry, offering a comprehensive range of solutions tailored for diverse needs. Specializing in interior and exterior paints, alongside a dedicated line of Marine paints, our company stands at the forefront of innovation and quality craftsmanship. With a commitment to excellence and customer satisfaction, Nerzpaint sets a new standard of reliability and performance in every brushstroke, making spaces come alive with vibrant colors and enduring beauty.</Text>      
        
               <WrapBox>
 
