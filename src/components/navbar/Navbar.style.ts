03bd0d228bb69f57a55c8ff2ecf452e47274f7c5
Onyechere Chukwuemeka Favour <81271549+Chuboii@users.noreply.github.com>
2024-03-14 09:40:44 +0100

    Update Navbar.style.ts

--- src/components/navbar/Navbar.style.ts
+++ src/components/navbar/Navbar.style.ts
@@ -19,7 +19,7 @@ export const Container = styled.nav<ContainerType>`
     background-color: ${({bg}) => bg};
     align-items: center;
     display:flex;
-    z-index: 9;
+    z-index: 1000;
 
     @media screen and (max-width:1028px){
         padding:.5rem 1rem;
